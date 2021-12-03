8f80d93947d3b443e1bda3f1fc75a34d18499c2e
Chuck Ludwig Reina <chuck@Chucks-MacBook-Pro.local>
2021-12-02 18:05:36 -0600

    testing using form post

--- noom-app/src/Controller/Controller.ts
+++ noom-app/src/Controller/Controller.ts
@@ -1,9 +1,21 @@
 export class Controller {
     static createRequest(arg) {
-        fetch("https://api.particle.io/v1/devices/e00fce68873438d598031f19/led?access_token=1cf158060ec1b7c46ad748774002ef19ae1db657", 
+      let obj = {"arg":arg}
+      var formBody:any = [];
+      for (var property in obj) {
+        var encodedKey = encodeURIComponent(property);
+        var encodedValue = encodeURIComponent(obj[property]);
+        formBody.push(encodedKey + "=" + encodedValue);
+      }
+      formBody = formBody.join("&");
+
+
+      fetch("https://api.particle.io/v1/devices/e00fce68873438d598031f19/led?access_token=1cf158060ec1b7c46ad748774002ef19ae1db657", 
         {
             method:"POST",   
-          body:JSON.stringify(arg)})
+            headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }, body:formBody})
           .then(r => console.log("Updated", r) )
           .catch(e => { console.warn("Error Updating", e);
           })
